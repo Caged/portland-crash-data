674916f885ce0bedab7d20d74dc4dcc7f6694f0d
Justin Palmer <justin@github.com>
2014-07-23 22:58:04 -0700

    remove individual exports.  We do this inline now.

--- export/rdwy.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `RDWY` (`RDWY_NO`, `RDWY_DESC`, `RDWY_TERMNT_DT`, `LAST_UD_DT`, `LAST_UD_USER_ID`) VALUES ("1","Add-mileage on divided highway or an undivided hwy",NULL,"03/20/02 00:00:00","CONVERT ");
-INSERT INTO `RDWY` (`RDWY_NO`, `RDWY_DESC`, `RDWY_TERMNT_DT`, `LAST_UD_DT`, `LAST_UD_USER_ID`) VALUES ("2","Non-add alignment on divided highway or couplet",NULL,"03/20/02 00:00:00","CONVERT ");
-INSERT INTO `RDWY` (`RDWY_NO`, `RDWY_DESC`, `RDWY_TERMNT_DT`, `LAST_UD_DT`, `LAST_UD_USER_ID`) VALUES ("3","Add-mileage alignment of a split roadway ","12/31/09 00:00:00","03/01/13 14:00:36","Hwye85p ");
-INSERT INTO `RDWY` (`RDWY_NO`, `RDWY_DESC`, `RDWY_TERMNT_DT`, `LAST_UD_DT`, `LAST_UD_USER_ID`) VALUES ("4","Non-add mileage alignment of a split roadway ","12/31/09 00:00:00","03/01/13 14:00:36","Hwye85p ");
-INSERT INTO `RDWY` (`RDWY_NO`, `RDWY_DESC`, `RDWY_TERMNT_DT`, `LAST_UD_DT`, `LAST_UD_USER_ID`) VALUES ("5","Alignment not yet built or non-state rdwy ",NULL,"03/20/02 00:00:00","CONVERT ");
